567ac27dbf0f699a34e0b6f6f1428eb202ffbe24
jpb06 <jpb.06@outlook.com>
2024-03-25 22:34:34 +0100

    fix: mobile responsiveness

--- src/components/organisms/devs-list/DevsList.tsx
+++ src/components/organisms/devs-list/DevsList.tsx
@@ -14,7 +14,7 @@ export const DevsList = async ({ searchParams }: DevsListProps) => {
   const selectedSquads = getSelectedSquadsFrom(searchParams);
   const devs = await DevFriendsApi.devsBySquads(selectedSquads);
 
-  if (!devs?.length && selectedSquads.length > 0) {
+  if (!devs?.length) {
     return (
       <div className="flex flex-col items-center rounded-xl p-4">
         <FaceFrownIcon className="h-20 w-20 text-slate-500" />
@@ -24,7 +24,7 @@ export const DevsList = async ({ searchParams }: DevsListProps) => {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 xl:grid-cols-4">
+    <div className="grid w-full grid-cols-1 gap-2 sm:grid-cols-3 md:grid-cols-4">
       {devs.map((dev) => (
         <Dev key={dev.id} {...dev} />
       ))}
